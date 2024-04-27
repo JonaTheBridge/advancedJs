81aa7167367e0b82d95873220e65cd06d031c6a0
JonaTheBridge <jonathan@thebridgeschool.es>
2024-04-27 11:26:14 +0200

    include try/catch to promises class

--- 06) promises/class.js
+++ 06) promises/class.js
@@ -1,12 +1,30 @@
 // fetch('https://restcountries.com/v3.1/all')
+// fetch('https://wrongRequestToFakeApi.com/')
 //   .then(res => res.json())
 //   .then(formattedResponse => console.log('formattedResponse', formattedResponse))
-//   .catch(err => console.error(err));
+//   .catch(err => console.error('ERROR!', err));
 
-// async function getCountries() {
-//   const response = await fetch('https://restcountries.com/v3.1/all');
-//   const data = await response.json();
-//   console.log('async/await', data);
-// }
+async function getCountries() {
+  try {
+    // const response = await fetch('https://restcountries.com/v3.1/all');
+    const response = await fetch('https://wrongRequestToFakeApi.com/');
+    const data = await response.json();
+    console.log('async/await', data);
+  } catch(err) {
+    // console.error('ERROR!', err);
+    // console.error('JSON.stringify(ERROR!)', JSON.stringify(err));
+    const { cause, } = err;
+    const message = `ERROR: Request to ${cause.hostname} response with code ${cause.code}`;
+    throw new Error(message);
+  }
+}
 
-// getCountries();
+async function runProgram() {
+  try {
+    await getCountries();
+  } catch(myError) {
+    console.error('myError', myError.message);
+  }
+}
+
+runProgram();
